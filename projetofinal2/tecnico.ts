d1f3bc6cb4642720f843d4af1c23a084b9687119
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-12-13 22:18:32 -0300

    Add files via upload

---
+++ projetofinal2/tecnico.ts
@@ -0,0 +1,15 @@
+class Tecnico {
+    private id : number;
+    private nome : string ;
+    private lutadores : [] ; 
+
+    constructor(id : number ,usuario:string ) {
+        this.id = id;
+        this.nome = usuario;
+    }
+
+    public encontrar_lutador(): void {
+        
+    }
+}
+
